dec7034e2da2d4470d5875b8cb7e82ba7f7b5b2a
Rahul Tarak <rahultarak12345@gmail.com>
2017-09-20 13:51:06 +0000

    Post Working

--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php 
-$var = $_REQUEST['temp'];
-if($var == "12345"){
-    echo "You are a nooberest";
-}
+header('Access-Control-Allow-Origin: *'); 
+  $username = $_REQUEST['email'];
+  $password = $_REQUEST['pword'];
+  if ($username == "rahultarak12345@gmail.com") {
+      echo "Sucessful Login";
+  }
 ?>
