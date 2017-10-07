5e42ac43a69c0b9d43002e42fec79166e09f2ec6
Rahul Tarak <rahultarak12345@gmail.com>
2017-10-07 14:30:04 +0000

    Cleaned up code - using angular syntax for ajax and getting values

--- js/mycopy.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("quickbooks", ["ngRoute"]);
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "homepage.html"
-    });
-  });
