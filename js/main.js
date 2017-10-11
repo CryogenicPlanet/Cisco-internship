1aa07816b0c2f7d695a69a7dfbfff16cb29136dc
Rahul Tarak <rahultarak12345@gmail.com>
2017-10-11 16:24:04 +0000

    Js Changes

--- js/main.js
+++ js/main.js
@@ -17,18 +17,28 @@ app.config(function($routeProvider) {
 app.controller('loginController', function($scope, $http, $location) {
     $scope.login = function() {
             var url = "https://cisco-backend-cryogenicplanet.c9users.io/login";
-            url = "/test.php";
+            var data = {email: $scope.lEmail, pword: $scope.lPassword};
             $http({
                 method : "POST",
-                datatype : "json",
-                data: $.param({email: $scope.lEmail, pword: $scope.lPassword}),
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+               contentType: 'application/json',
+                data : JSON.stringify(data),
+                cache: true,
                 url : url
-            }).then(function mySuccess(response) {
+            }).then(/*function mySuccess(response) {
                 $scope.success = response.data;
                 $location.path("/home");
             }, function myError(response) {
                 $scope.error = response.statusText;
-            });
-        }
-});
+            } */ function sucess(response){
+                console.log(response.data);
+                 if (response.data.status == true){
+             $scope.success = response.data;
+                $location.path("/home");
+    } else {
+      // Show the whole page
+       Materialize.toast('<p class="flow-text white-text">' + response.data.message + '</p>', 2000);
+        $scope.error = response.statusText;
+      
+    }});
+        
+}});
