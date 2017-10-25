2b93f5cf5be9a44848f07a7489ec2582e3deb7a2
Rahul Tarak <rahultarak12345@gmail.com>
2017-10-25 14:25:05 +0000

    Added JWT(Fixing Idiotic Mistake) and Added Cached Memory

--- js/main.js
+++ js/main.js
@@ -1,85 +1,132 @@
 var app = angular.module("quickbooks", ["ngRoute"]);
-
+// Route
 app.config(function($routeProvider) {
-  
-   $routeProvider
-    .when("/", {
-        templateUrl : "login.html",
-        controller: "loginController"
-    })
-    .when("/home", {
-        templateUrl : "homepage.html",
-        controller: "homeController"
-    });
+
+    $routeProvider
+        .when("/login", {
+            templateUrl: "login.html",
+            controller: "loginController"
+        })
+        .when("/", {
+            templateUrl: "homepage.html",
+            controller: "homeController"
+        });
 });
 
+// Factory
 app.factory('userService', function($http) {
     var userService = {};
-    userService.uuid = -1;
-    userService.login = function(email, password){
+    userService.login = function(email, password) {
         var url = "https://cisco-backend-cryogenicplanet.c9users.io/login";
-        var data = {email: email, pword: password};
+        var data = { email: email, pword: password };
         return $http({
-            method : "POST",
+            method: "POST",
             contentType: 'application/json',
-            data : JSON.stringify(data),
+            data: JSON.stringify(data),
             cache: true,
-            url : url
-        }).then(function (response){
-           if(response.data.status == true){
-               userService.uuid = response.data.uuid;
-                return response.data.uuid;
-           } else {
-               throw Error(response.data.message);
-           }
-        });
+            url: url
+        }).then(function(response) {
+            localStorage.setItem("token", response.data.token);
+                sessionStorage.setItem('token', response.data.token);
+                return response.data.token;
+        })
+        .catch(function(response){ 
+        
+                throw Error(response.data.message);
+            });
     };
-    userService.getUuid = ()=>{return userService.uuid};
+    userService.getToken = () => { return localStorage.getItem("token");};
     return userService;
 });
-app.service('newBooksService',function($http, userService){
+// Services
+app.service('newBooksService', function($http, userService) {
     var newBooks = [];
-    newBooks.get = function(){
-       return  $http({
-            method : "GET",
-            url : `https://cisco-backend-cryogenicplanet.c9users.io/newbooks?uuid=` + userService.getUuid()
-             /* headers : {'Accept' : 'application/json'},
-            params: JSON.stringify({uuid:userId}) */
-        }).then(function(responses){
+    newBooks.get = function() {
+        return $http({
+            method: "GET",
+            url: `https://cisco-backend-cryogenicplanet.c9users.io/newbooks`,
+             headers : {'Content-Type' : 'application/json','x-access-token' : userService.getToken()},
+                    }).then(function(responses) {
+            newBooks.responses = responses.data;
             return responses.data;
-        })
+        });
     }
+   // newBooks.getResponses = () => { return newBooks.responses };
     return newBooks;
 });
+app.service('borrowService', function($http, userService) {
+    var borrowService = [];
+    borrowService.borrow = function(lender, ubid) {
+        var data = {
+            lender: lender,
+            ubid: ubid
+        };
+        console.log(data);
+        return $http({
+            method: "POST",
+             headers : {'Content-Type' : 'application/json','x-access-token' : userService.getToken()},
+            data: JSON.stringify(data),
+            cache: true,
+            url: `https://cisco-backend-cryogenicplanet.c9users.io/borrow`
+        });
+    };
+    return borrowService;
+});
+
+// Controllers
+app.controller('borrowController', function($scope, borrowService) {
+   
+});
+
 
 app.controller('baseController', function($scope, userService) {
-    
+
 });
 
 app.controller('loginController', function($scope, $http, $location, userService, newBooksService) {
     $scope.login = function() {
         userService.login($scope.lEmail, $scope.lPassword)
-        .then(function(uuid) {
-            $location.path("/home");
-        })
-        .catch(function(error) {
+            .then(function(token) {
+                $location.path("/home");
+            })
+            .catch(function(error) {
                 // Show the whole page
-            Materialize.toast('<p class="flow-text white-text">' + error + '</p>', 2000);            
-            
-        });
+                Materialize.toast('<p class="flow-text white-text">' + error + '</p>', 2000);
+
+            });
     };
-    console.log('test');
+
+    //console.log('test');
 });
 
 
-app.controller('homeController', function($scope, newBooksService) {
+app.controller('homeController', function($scope,$location, newBooksService,borrowService,userService) {
     console.log("Homepage!");
-    $scope.initialize = function() {
+    $scope.initialize = function() { // Function Defined here
+    var token = userService.getToken();
+    if(token){
+        console.log(token);
         newBooksService.get()
-        .then(function(responses){
-            $scope.books = responses;
-            console.log($scope.books);
-        });
+            .then(function(responses) {
+                $scope.books = responses;
+                console.log($scope.books);
+            });
+    } else {
+        $location.path("/")
+    }
+        
     };
+     $scope.borrow = function(book) {
+        borrowService.borrow(book.uuid,book.ubid)
+            .then(function(response) {
+                console.log("sucess");
+                Materialize.toast('<p class="flow-text white-text">' + response.data.message + '</p>', 2000);
+
+            })
+            .catch(function(error) {
+                Materialize.toast('<p class="flow-text red-text">' + error.data.message + '</p>', 2000);
+            });
+    };
+    //Here
     $scope.initialize();
 });
