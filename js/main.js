f393436c9172935ee8e54366fd22ad2936956613
Rahul Tarak <rahultarak12345@gmail.com>
2017-10-30 16:47:58 +0000

    Latest Push

--- js/main.js
+++ js/main.js
@@ -10,32 +10,66 @@ app.config(function($routeProvider) {
         .when("/", {
             templateUrl: "homepage.html",
             controller: "homeController"
+        })
+        .when("/loading", {
+            templateUrl: "loading.html",
+            controller: "loadingController"
+        })
+        .when("/username", {
+            templateUrl: "user.html",
+            controller: "userController"
         });
 });
-
+app.directive('handleLink', function() {
+    var el = document.getElementById(attrs.href);
+    el.scrollIntoView();
+});
 // Factory
 app.factory('userService', function($http) {
     var userService = {};
+    var username;
     userService.login = function(email, password) {
         var url = "https://cisco-backend-cryogenicplanet.c9users.io/login";
         var data = { email: email, pword: password };
         return $http({
-            method: "POST",
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            cache: true,
-            url: url
-        }).then(function(response) {
-            localStorage.setItem("token", response.data.token);
+                method: "POST",
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                cache: true,
+                url: url
+            }).then(function(response) {
+                localStorage.setItem("token", response.data.token);
                 sessionStorage.setItem('token', response.data.token);
+                userService.getDetails();
                 return response.data.token;
-        })
-        .catch(function(response){ 
-        
+            })
+            .catch(function(response) {
+
                 throw Error(response.data.message);
             });
     };
-    userService.getToken = () => { return localStorage.getItem("token");};
+    userService.getDetails = function() {
+        return $http({
+            method: "GET",
+            url: `https://cisco-backend-cryogenicplanet.c9users.io/userDetails`,
+            headers: { 'Content-Type': 'application/json', 'x-access-token': userService.getToken() },
+        }).then(function(responses) {
+            username = responses.data.response.name;
+            localStorage.setItem("username", username);
+            var usersBooks = JSON.stringify(responses.data.response.books);
+            var followers = JSON.stringify(responses.data.response.followers);
+            var following = JSON.stringify(responses.data.response.following);
+            sessionStorage.setItem("books", usersBooks);
+            sessionStorage.setItem("followers", followers);
+            sessionStorage.setItem("following", following);
+            return responses.data;
+        });
+    }
+    userService.getBooks = () => { return JSON.parse(sessionStorage.getItem("books")); };
+    userService.getFollowers = () => { return JSON.parse(sessionStorage.getItem("followers")); };
+    userService.getFollowing = () => { return JSON.parse(sessionStorage.getItem("following")); };
+    userService.getUsername = () => { return localStorage.getItem("username"); };
+    userService.getToken = () => { return localStorage.getItem("token"); };
     return userService;
 });
 // Services
@@ -45,13 +79,13 @@ app.service('newBooksService', function($http, userService) {
         return $http({
             method: "GET",
             url: `https://cisco-backend-cryogenicplanet.c9users.io/newbooks`,
-             headers : {'Content-Type' : 'application/json','x-access-token' : userService.getToken()},
-                    }).then(function(responses) {
+            headers: { 'Content-Type': 'application/json', 'x-access-token': userService.getToken() },
+        }).then(function(responses) {
             newBooks.responses = responses.data;
             return responses.data;
         });
     }
-   // newBooks.getResponses = () => { return newBooks.responses };
+    // newBooks.getResponses = () => { return newBooks.responses };
     return newBooks;
 });
 app.service('borrowService', function($http, userService) {
@@ -64,7 +98,7 @@ app.service('borrowService', function($http, userService) {
         console.log(data);
         return $http({
             method: "POST",
-             headers : {'Content-Type' : 'application/json','x-access-token' : userService.getToken()},
+            headers: { 'Content-Type': 'application/json', 'x-access-token': userService.getToken() },
             data: JSON.stringify(data),
             cache: true,
             url: `https://cisco-backend-cryogenicplanet.c9users.io/borrow`
@@ -75,7 +109,7 @@ app.service('borrowService', function($http, userService) {
 
 // Controllers
 app.controller('borrowController', function($scope, borrowService) {
-   
+
 });
 
 
@@ -83,11 +117,19 @@ app.controller('baseController', function($scope, userService) {
 
 });
 
-app.controller('loginController', function($scope, $http, $location, userService, newBooksService) {
+app.controller('loginController', function($scope, $http, $location, $timeout, userService, newBooksService) {
+    this.initialize = function() {
+        $scope.loading = true;
+        $scope.loginPage = false;
+        $timeout(function() {
+            $scope.loading = false;
+            $scope.loginPage = true;
+        }, 3000);
+    }
     $scope.login = function() {
         userService.login($scope.lEmail, $scope.lPassword)
             .then(function(token) {
-                $location.path("/home");
+                $location.path("/");
             })
             .catch(function(error) {
                 // Show the whole page
@@ -95,31 +137,43 @@ app.controller('loginController', function($scope, $http, $location, userService
 
             });
     };
-
+    this.initialize();
     //console.log('test');
 });
 
 
-app.controller('homeController', function($scope,$location, newBooksService,borrowService,userService) {
+app.controller('homeController', function($scope, $location, newBooksService, borrowService, userService) {
     console.log("Homepage!");
+    $scope.homepage = false;
     $scope.initialize = function() { // Function Defined here
-    var token = userService.getToken();
-    if(token){
-        console.log(token);
-        newBooksService.get()
-            .then(function(responses) {
-                $scope.books = responses;
-                console.log($scope.books);
-            });
-    } else {
-        $location.path("/")
-    }
-        
+        var token = userService.getToken();
+        $scope.loading = true;
+        $scope.showBooks = false;
+        if (token) {
+            $scope.homepage = true;
+            console.log(token);
+            newBooksService.get()
+                .then(function(responses) {
+                    $scope.books = responses;
+                    $scope.showBooks = true;
+
+                }).catch(function(err) {
+                    Materialize.toast('<p class="flow-text red-text">' + err.data.message + '</p>', 2000);
+                })
+                .finally(function() {
+                    // called no matter success or failure
+                    $scope.loading = false;
+                });
+        }
+        else {
+            $location.path("/login")
+        }
+
     };
-     $scope.borrow = function(book) {
-        borrowService.borrow(book.uuid,book.ubid)
+    $scope.borrow = function(book) {
+        borrowService.borrow(book.uuid, book.ubid)
             .then(function(response) {
-                console.log("sucess");
+                console.log("success");
                 Materialize.toast('<p class="flow-text white-text">' + response.data.message + '</p>', 2000);
 
             })
@@ -130,3 +184,32 @@ app.controller('homeController', function($scope,$location, newBooksService,borr
     //Here
     $scope.initialize();
 });
+app.controller('userController', function($scope, userService) {
+    $scope.userPage = false;
+    $scope.loading = true;
+    $scope.initialize = function() {
+        console.log(userService.getUsername());
+        $scope.name = userService.getUsername();
+        $scope.books = userService.getBooks();
+        $scope.following = userService.getFollowing();
+        $scope.followers = userService.getFollowers();
+
+    }
+    if (!(userService.getBooks())) {
+        userService.getDetails()
+            .then(function(data) {
+                console.log("getDetails Succesfull");
+                console.log(JSON.parse(sessionStorage.getItem("followers")));
+                $scope.initialize();
+            })
+            .finally(function(data) {
+                $scope.loading = false;
+                $scope.userPage = true;
+            });
+    }
+    else {
+        $scope.initialize();
+        $scope.loading = false;
+        $scope.userPage = true;
+    }
+})
